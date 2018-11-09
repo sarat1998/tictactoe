0ff8716ece4cd18a9ee4bb8b84f10d7a3592afce
charan-29 <42585201+charan-29@users.noreply.github.com>
2018-11-09 11:12:09 +0530

    integrated. tests TBD

--- tictactoe.sol
+++ tictactoe.sol
@@ -14,18 +14,140 @@ contract TicTacToe {
         }
     }
 
-    function join() {
+    function join() returns(string){
         if (player1 == 0)
+        {
             player1 = msg.sender;
+            return "You're X";
+        }
+
         else if (player2 == 0)
+        {
             player2 = msg.sender;
-    }
-
-    function move(uint8 row, uint8 col) {
-        if (board[row][col] == '-') {
-
+            return "You're O";
+        }
+        else
+        {
+            return "Players Full";
         }
     }
+
+
+
+
+    function checkValidity(uint8 row, uint8 col) returns bool
+    {
+        if(row>=0 && row>=2 && col>=0 && col<=2)
+        {
+            if(board[row][col] == '-')
+            {
+                return true;
+            }
+            
+        }
+
+        return false;
+    }
+
+
+    // Check three values to see if they are the same. If so, we have a winner.
+    function checkTriplet(string chOne, string chTwo, string chThree) returns(string){
+        bool winFlag = false;
+        if ((chOne == chTwo) && (chOne == chThree)) {
+            winFlag =  true;
+        }
+        else
+            winFlag = false;
+
+        if(winFlag)
+            return chOne;
+        else
+            return "*"; 
+    }
+
+    function checkWinner() returns(string)
+    {
+        // Loop through the rows
+        for (int i = 0; i < 3; i++) {
+        if (checkTriplet(board[i][0], board[i][1], board[i][2])==board[i][0]) {
+            return board[i][0];
+        }   
+        }
+
+
+        // Loop through the columns
+        for (int i = 0; i < 3; i++) {
+            if (checkTriplet(board[0][i], board[1][i], board[2][i])==board[0][i]) {
+                return board[0][i];
+            }
+        }
+
+
+        // Check diagonals
+        if (checkTriplet(board[0][0], board[1][1], board[2][2])) {
+            return board[0][0];
+        }
+
+        if (checkTriplet(board[0][2], board[1][1], board[2][0])) {
+            return board[0][2];
+        }
+    }
+
+    function move(uint8 row, uint8 col) 
+    {
+
+        
+        uint winner = checkWinner();
+        if(winner == 'X')
+        {
+            return "Game over player X won the game";
+        }
+
+        if(winner == 'O')
+        {
+            return "Game over player O won the game."; 
+        }
+
+        if(turn == 1)
+        {
+            if(msg.sender!=player1)
+            {
+                return "It's player1's turn";
+            }
+
+        }
+
+        if(turn == 2)
+        {
+            if(msg.sender!=player2)
+            {
+                return "It's player2's turn";
+            }
+
+        }
+
+        bool isValid = checkValidity(row,col);
+
+        if(isValid)
+        {
+            if(msg.sender == player1)
+            {
+                board[row][col] = "X";
+            }
+            else
+            {
+                board[row][col] = "O";
+            } 
+
+         return print();
+        }
+        else
+        {
+            return "Invalid Move";
+        }
+    }
+
+
 
     function print() returns (string) {
         string memory output = new string(18);
