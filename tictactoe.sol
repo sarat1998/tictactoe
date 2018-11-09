463f43d5e875304636f2b2a9dc52748dd1739d80
charan-29 <42585201+charan-29@users.noreply.github.com>
2018-11-09 11:30:20 +0530

    some changes string-> bytes32

--- tictactoe.sol
+++ tictactoe.sol
@@ -1,8 +1,10 @@
 pragma solidity ^0.4.0;
 
 contract TicTacToe {
-    string[3][3] board;
-    address manager, player1 = 0, player2 = 0;
+    bytes32[3][3] board;
+    address manager;
+    address player1 = 0;
+    address player2 = 0;
     uint8 turn = 1;
 
     function TicTacToe() {
@@ -35,11 +37,11 @@ contract TicTacToe {
 
 
 
-    function checkValidity(uint8 row, uint8 col) returns bool
+    function checkValidity(uint8 row, uint8 col) returns (bool)
     {
         if(row>=0 && row>=2 && col>=0 && col<=2)
         {
-            if(board[row][col] == '-')
+            if( keccak256(board[row][col]) == keccak256("-") )
             {
                 return true;
             }
@@ -51,9 +53,9 @@ contract TicTacToe {
 
 
     // Check three values to see if they are the same. If so, we have a winner.
-    function checkTriplet(string chOne, string chTwo, string chThree) returns(string){
+    function checkTriplet(bytes32 chOne, bytes32 chTwo, bytes32 chThree) returns(bytes32){
         bool winFlag = false;
-        if ((chOne == chTwo) && (chOne == chThree)) {
+        if ((keccak256(chOne) == keccak256(chTwo)) && (keccak256(chOne) == keccak256(chThree))) {
             winFlag =  true;
         }
         else
@@ -62,48 +64,48 @@ contract TicTacToe {
         if(winFlag)
             return chOne;
         else
-            return "*"; 
+            return bytes32("*"); 
     }
 
-    function checkWinner() returns(string)
+    function checkWinner() returns(bytes32)
     {
         // Loop through the rows
-        for (int i = 0; i < 3; i++) {
-        if (checkTriplet(board[i][0], board[i][1], board[i][2])==board[i][0]) {
+        for (uint8 i = 0; i < 3; i++) {
+        if ( keccak256(checkTriplet(board[i][0], board[i][1], board[i][2]))==keccak256(board[i][0])) {
             return board[i][0];
         }   
         }
 
 
         // Loop through the columns
-        for (int i = 0; i < 3; i++) {
-            if (checkTriplet(board[0][i], board[1][i], board[2][i])==board[0][i]) {
-                return board[0][i];
+        for (uint8 i2 = 0; i2 < 3; i2++) {
+            if (keccak256(checkTriplet(board[0][i2], board[1][i2], board[2][i2]))==keccak256(board[0][i2])) {
+                return board[0][i2];
             }
         }
 
 
         // Check diagonals
-        if (checkTriplet(board[0][0], board[1][1], board[2][2])) {
+        if (    keccak256(checkTriplet(board[0][0], board[1][1], board[2][2])) == keccak256(board[0][0]) ) {
             return board[0][0];
         }
 
-        if (checkTriplet(board[0][2], board[1][1], board[2][0])) {
+        if (keccak256(checkTriplet(board[0][2], board[1][1], board[2][0]))==keccak256(board[0][2])) {
             return board[0][2];
         }
     }
 
-    function move(uint8 row, uint8 col) 
+    function move(uint8 row, uint8 col) returns(bytes32[])
     {
 
         
-        uint winner = checkWinner();
-        if(winner == 'X')
+        bytes32 winner = checkWinner();
+        if(keccak256(winner) == keccak256("X"))
         {
             return "Game over player X won the game";
         }
 
-        if(winner == 'O')
+        if(keccak256(winner) == keccak256("O"))
         {
             return "Game over player O won the game."; 
         }
@@ -149,13 +151,13 @@ contract TicTacToe {
 
 
 
-    function print() returns (string) {
-        string memory output = new string(18);
+    function print() returns (bytes32[]) {
+        bytes32[] memory output = new bytes32[](18);
         uint8 idx = 0;
 
         for (uint8 i = 0; i < 3; i++) {
             for (uint8 j = 0; j < 3; j++) {
-                output[idx++] = board[i][j];
+                output[idx++] = bytes32(board[i][j]);
                 output[idx++] = '|';
             }
             output[idx++] = '\n';
